7eb4b48b9f39f83d8087c1b186a658bed719ecbe
ekostyukova <kostyukova_ea@mail.ru>
2025-11-16 14:36:34 +0500

    Initial commit

---
+++ src/test/java/DemoQAPageObjectsTest.java
@@ -0,0 +1,73 @@
+import org.junit.jupiter.api.Test;
+import pages.Component.ResultComponent;
+import pages.RegistrationPage;
+
+
+public class DemoQAPageObjectsTest extends TestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
+    ResultComponent resultComponent = new ResultComponent();
+
+    @Test
+    void demoFormTest() {
+        registrationPage.openPage()
+                .setFirstName("Sergey")
+                .setLastName("Redko")
+                .setUserEmail("redkoSS@mail.ru")
+                .setGender("Male")
+                .setPhoneNumber("8345265897")
+                .setOfBirth("15", "April", "1990")
+                .setSubjects("Maths")
+                .setHobbies("Sports")
+                .setHobbies("Reading")
+                .setPicture("photo_2025.jpg")
+                .setCurrentAddress("Russia")
+                .setState("Haryana")
+                .setCity("Panipat")
+                .submitForm();
+
+        resultComponent.checkResult("Student Name", "Sergey Redko")
+                .checkResult("Student Email", "redkoSS@mail.ru")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "8345265897")
+                .checkResult("Date of Birth", "15 April,1990")
+                .checkResult("Subjects", "Maths")
+                .checkResult("Hobbies", "Sports, Reading")
+                .checkResult("Picture", "photo_2025.jpg")
+                .checkResult("Address", "Russia")
+                .checkResult("State and City", "Haryana Panipat");
+    }
+
+    @Test
+    void demoFormTestMinimumData() {
+        registrationPage.openPage()
+                .setFirstName("Sergey")
+                .setLastName("Redko")
+                .setGender("Male")
+                .setPhoneNumber("8345265897")
+                .setState("Haryana")
+                .setCity("Panipat")
+                .submitForm();
+
+        resultComponent.checkResult("Student Name", "Sergey Redko")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "8345265897")
+                .checkResult("State and City", "Haryana Panipat");
+    }
+
+    @Test
+    void demoFormTestNegativeCheck() {
+        registrationPage.openPage()
+                .setFirstName("Sergey")
+                .setLastName("Redko")
+                .setUserEmail("redkoSSmail.ru")
+                .setGender("Male")
+                .setPhoneNumber("8345265897")
+                .setState("Haryana")
+                .setCity("Panipat")
+                .submitForm();
+
+        resultComponent.closeNot();
+    }
+}
+
+
---
+++ src/test/java/TestBoxTests.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import pages.TestBoxPage;
+
+public class TestBoxTests extends TestBase {
+
+    TestBoxPage testBoxPage = new TestBoxPage();
+
+    @Test
+    void demoTest() {
+        testBoxPage.openPage()
+                .setUserName("Ivanov Ivan")
+                .setEmailName("Ivanov@mail.ru")
+                .setCurrentAddress("Kazan")
+                .setPermanentAddress("Moscow")
+                .submitForm();
+
+        testBoxPage.checkResult("Name:", "Ivanov Ivan")
+                .checkResult("Email:", "Ivanov@mail.ru")
+                .checkResult("Current Address :", "Kazan")
+                .checkResult("Permananet Address :", "Moscow");
+    }
+}
